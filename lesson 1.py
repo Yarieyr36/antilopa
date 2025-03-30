e90c723371dfa5057a92cd9b95a677f3ebb994b2
Yarieyr36 <sucylynaroslav45@gmail.com>
2025-03-30 11:34:53 +0300

    Well, there's a place to write your name, get a passport, and all that.

---
+++ lesson 1.py
@@ -0,0 +1,21 @@
+from xml.dom.minidom import ProcessingInstruction
+'''
+print("Hello World")
+print(2+2*2)
+print("Big  " + "Nia")
+print(" Nia " *7 )
+'''
+# - однорядковий коментар
+'''
+багаторядковий
+   коментар
+'''
+
+print("Мені",14,"рочків")
+name = input("Enter your name:")
+print("Hello", name)
+age = int(input("Enter your age:"))
+if age>=14:
+    print("here is your passport.")
+else:
+    print("bruh, grow up bro💀")
