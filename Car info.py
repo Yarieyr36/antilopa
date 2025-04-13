7f3d2042adbac235857b522eef47c3f20bd4dff4
Yarieyr36 <sucylynaroslav45@gmail.com>
2025-04-13 08:33:40 +0300

    Car info, year, mark, model.

---
+++ Car info.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, mark, model, year):
+        self.mark = mark
+        self.model = model
+        self.year = year
+
+    def getinfo (self):
+        return f"{self.year} {self.mark} {self.model}"
+
+My_car = Car("Audi", "A4", 2021)
+print(My_car.getinfo())
